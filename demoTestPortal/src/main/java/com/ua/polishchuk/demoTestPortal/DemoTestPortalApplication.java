b1008a480718299416ac9eba22c5adfce2d00e00
Oleksii Polishchuk <Oleksii_Polishchuk@epam.com>
2019-06-15 22:22:15 +0300

    Start working

---
+++ demoTestPortal/src/main/java/com/ua/polishchuk/demoTestPortal/DemoTestPortalApplication.java
@@ -0,0 +1,13 @@
+package com.ua.polishchuk.demoTestPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestPortalApplication.class, args);
+	}
+
+}
---
+++ demoTestPortal/src/test/java/com/ua/polishchuk/demoTestPortal/DemoTestPortalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ua.polishchuk.demoTestPortal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoTestPortalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
